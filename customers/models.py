34416b5a12232ffef21bfd0937ff6409a6c47842
Lionnel patrick DOOKO <patrick.lionnel@finetechgroupe.com>
2024-07-24 19:46:58 +0000

    creation de l'application customer

---
+++ customers/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Customer(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=100)
+    job = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    account = models.OneToOneField('accounts.User', on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.firstname
+    
